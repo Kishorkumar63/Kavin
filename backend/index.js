4117f3c25185805559098f81cd91c656b9eeb31b
kishor kumar k <110096460+Kishorkumar63@users.noreply.github.com>
2024-01-26 00:38:27 +0530

    unComment

--- backend/index.js
+++ backend/index.js
@@ -38,18 +38,18 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// process.on("unhandledRejection", (err) => {
-//   console.log(`Error: ${err.message}`);
-//   console.log("Shutting down the server due to unhandled rejection error");
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to unhandled rejection error");
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
-// process.on("uncaughtException", (err) => {
-//   console.log(`Error: ${err.message}`);
-//   console.log("Shutting down the server due to uncaught exception error");
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception error");
+  server.close(() => {
+    process.exit(1);
+  });
+});
